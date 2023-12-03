a0ae1f9b12c2904dfc512566c983461ed48626d7
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-03 07:40:20 +0000

    Refactor and de-dupe.

--- 3/run.php
+++ 3/run.php
@@ -3,83 +3,78 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputMap();
 
-	$numbers = [];
-
-	function getSymbols($startx, $starty, $endx, $endy) {
-		global $input;
-
+	function getSymbols($input, $startx, $starty, $endx, $endy) {
 		$symbols = [];
 
 		foreach (yieldXY($startx, $starty, $endx, $endy, true) as $x => $y) {
 			$val = ($input[$y][$x] ?? '.');
 			if (!is_numeric($val) && $val != '.') {
-				$symbols[] = [$val, $x . ',' . $y];
+				if (!isset($symbols[$val])) { $symbols[$val] = []; }
+				$symbols[$val][] = $x . ',' . $y;
 			}
 		}
 
 		return $symbols;
 	}
 
-	$part1 = 0;
-	$gears = [];
-
-	foreach ($input as $y => $row) {
-		$current = null;
-		$startX = null;
-		foreach ($row as $x => $col) {
-			if ($current == null && is_numeric($col)) {
-				$startX = $x;
-				$current = $col;
-			} else if (is_numeric($col)) {
-				$current .= $col;
-			} else if ($current != null) {
-				$symbols = getSymbols($startX - 1, $y - 1, $x, $y + 1);
-				if (!empty($symbols)) {
-					$part1 += $current;
-					foreach ($symbols as $symbolInfo) {
-						[$symbol, $location] = $symbolInfo;
-
-						if ($symbol == '*') {
-							if (!isset($gears[$location])) { $gears[$location] = []; }
-							$gears[$location][] = $current;
-						}
-					}
+	function getNumberList($input) {
+		$numbers = [];
+		foreach ($input as $y => $row) {
+			$current = null;
+			$startx = null;
+			foreach ($row as $x => $col) {
+				if ($current == null && is_numeric($col)) {
+					$startx = $x;
+					$current = $col;
+				} else if (is_numeric($col)) {
+					$current .= $col;
+				} else if ($current != null) {
+					$numbers[] = [$current, $startx, $y];
+					$current = null;
 				}
+			}
 
-				debugOut('Number: ', $current, "\n");
-				debugOut("\t", (!empty($symbols) ? "VALID (" . implode('', array_column($symbols, 0)) . ")" : "NOT VALID"), "\n");
+			if ($current != null) {
+				$numbers[] = [$current, $startx, $y];
 				$current = null;
 			}
 		}
 
-		if ($current != null) {
-			$symbols = getSymbols($startX - 1, $y - 1, $x, $y + 1);
-			if (!empty($symbols)) {
-				$part1 += $current;
-				foreach ($symbols as $symbolInfo) {
-					[$symbol, $location] = $symbolInfo;
+		return $numbers;
+	}
 
-					if ($symbol == '*') {
+	$numbers = getNumberList($input);
+	$part1 = 0;
+	$gears = [];
+	foreach ($numbers as $number) {
+		[$number, $x, $y] = $number;
+		$endx = $x + (strlen($number) - 1);
+
+		$symbols = getSymbols($input, $x - 1, $y - 1, $endx + 1, $y + 1);
+		if (!empty($symbols)) {
+			$part1 += $number;
+			foreach ($symbols as $symbol => $locations) {
+				if ($symbol == '*') {
+					foreach ($locations as $location) {
 						if (!isset($gears[$location])) { $gears[$location] = []; }
-						$gears[$location][] = $current;
+						$gears[$location][] = $number;
 					}
 				}
 			}
+		}
 
-			debugOut('Number: ', $current, "\n");
-			debugOut("\t", (!empty($symbols) ? "VALID (" . implode('', array_column($symbols, 0)) . ")" : "NOT VALID"), "\n");
-			$current = null;
+		if (isDebug()) {
+			debugOut('Number: ', $number, ' => ');
+			debugOut((!empty($symbols) ? "VALID (" . implode('', array_keys($symbols)) . ")" : "NOT VALID"), "\n");
 		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
 
 	$part2 = 0;
-
 	foreach ($gears as $location => $numbers) {
 		if (count($numbers) == 2) {
 			$part2 += array_product($numbers);
 		}
 	}
-
 	echo 'Part 2: ', $part2, "\n";
