917cf019d22e4b8e77778da0df5fcdccac81298a
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-04 05:20:34 +0000

    Day 4. Part 2. 1922.

--- 4/run.php
+++ 4/run.php
@@ -4,6 +4,7 @@
 	$input = getInputLines();
 
 	$games = [];
+	$copies = [];
 	foreach ($input as $line) {
 		preg_match('#Card (.*): (.*) \| (.*)#SADi', $line, $m);
 		[$all, $gameId, $round1, $round2] = $m;
@@ -11,20 +12,33 @@
 		preg_match_all('/(\d+)/', $round1, $m1);
 		preg_match_all('/(\d+)/', $round2, $m2);
 
+		$gameId = trim($gameId);
 		$games[$gameId] = ['winning' => $m1[1], 'mine' => $m2[1]];
+		$copies[$gameId] = 1;
 	}
 
 	$part1 = 0;
 	foreach ($games as $gameId => $numbers) {
 		$value = 0;
+		$winning = 0;
 		foreach ($numbers['mine'] as $num) {
 			if (in_array($num, $numbers['winning'])) {
 				if ($value == 0) { $value = 1; }
 				else { $value += $value; }
+				$winning++;
 			}
 		}
 		$part1 += $value;
+
+		debugOut("{$gameId} had {$winning} matching numbers ($copies[$gameId] copies).\n");
+		for ($i = 0; $i < $winning; $i++) {
+			$newId = $gameId + 1 + $i;
+			if (isset($copies[$newId])) {
+				debugOut("\tAdded {$copies[$gameId]} copies of {$newId} from {$gameId}\n");
+				$copies[$newId] += $copies[$gameId];
+			}
+		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-//	echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', array_sum($copies), "\n";
