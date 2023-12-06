f12122626674606ab1e81536ca37ac037fb480a0
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-06 05:38:40 +0000

    Day 6. Part 2. 8494. I was very slow and dumb

--- 6/run.php
+++ 6/run.php
@@ -4,6 +4,7 @@
 	$input = getInputLines();
 
 	$races = [];
+	$megaRace = [];
 	foreach ($input as $line) {
 		$bits = explode(':', $line);
 		$type = $bits[0];
@@ -13,39 +14,84 @@
 		foreach ($m[1] as $id => $num) {
 			$races[$id][strtolower($type)] = $num;
 		}
+
+		$megaRace[strtolower($type)] = preg_replace('/[^0-9]/', '', $numbers);
 	}
 
 	function simulate($time, $hold) {
 		$distance = $hold * ($time - $hold);
-
-		debugOut("\t", $hold, ' => ', $distance);
-
 		return $distance;
 	}
 
 	function getWinningOptions($race) {
-		$winning = [];
-		for ($i = 0; $i <= $race['time']; $i++) {
-			$final = simulate($race['time'], $i);
+		$lower = findLower($race);
+		$higher = findHigher($race);
 
-			if ($final > $race['distance']) {
-				debugOut(' => Winner!');
-				$winning[] = $i;
+		return $higher - $lower + 1;
+	}
+
+	function findLower($race) {
+		$time = $race['time'];
+		$distance = $race['distance'];
+
+		$low = 0;
+		$high = $time - 1;
+
+		while ($low <= $high) {
+			$mid = floor(($low + $high) / 2);
+
+			$thisResult = (simulate($time, $mid) > $distance);
+			$lowerResult = (simulate($time, $mid - 1) > $distance);
+			$isLowest = ($thisResult && !$lowerResult);
+
+			if ($isLowest) {
+				return $mid;
 			}
 
-			debugOut("\n");
+			if ($thisResult) {
+				$high = $mid -1;
+			} else {
+				$low = $mid + 1;
+			}
 		}
 
-		return $winning;
+		return false;
+	}
+
+	function findHigher($race) {
+		$time = $race['time'];
+		$distance = $race['distance'];
+
+		$low = 0;
+		$high = $time - 1;
+
+		while ($low <= $high) {
+			$mid = floor(($low + $high) / 2);
+
+			$thisResult = (simulate($time, $mid) > $distance);
+			$upperResult = (simulate($time, $mid + 1) > $distance);
+			$isHighest = ($thisResult && !$upperResult);
+
+			if ($isHighest) {
+				return $mid;
+			}
+
+			if (!$thisResult) {
+				$high = $mid -1;
+			} else {
+				$low = $mid + 1;
+			}
+		}
+
+		return false;
 	}
 
 	$part1 = 1;
 	foreach ($races as $raceid => $race) {
-		echo json_encode($race), "\n";
-		$part1 *= count(getWinningOptions($race));
+		$part1 *= getWinningOptions($race);
 	}
 
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = getWinningOptions($megaRace);
+	echo 'Part 2: ', $part2, "\n";
