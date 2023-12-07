c9a2a5ef246adbed02262e9940cc23db201db42d
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-07 08:08:20 +0000

    Day 7. Part 2. 11754

--- 7/run.php
+++ 7/run.php
@@ -41,6 +41,7 @@
 		case Three = '3';
 		case Two = '2';
 		case One = '1';
+		case Joker = '0';
 
 		function getNumericValue(): int {
 			return match($this) {
@@ -58,6 +59,7 @@
 				CardType::Three => 3,
 				CardType::Two => 2,
 				CardType::One => 1,
+				CardType::Joker => 0,
 				default => throw new Exception('No numeric value for: ' . $this->value),
 			};
 		}
@@ -86,39 +88,111 @@
 		return HandType::HighCard;
 	}
 
-	$entries = [];
-	foreach ($input as $line) {
-		preg_match('#(.*) (.*)#SADi', $line, $m);
-		[$all, $handStr, $bid] = $m;
+	function getPossibleHands($handWithoutJokers) {
+		if (count($handWithoutJokers) >= 5) { return $handWithoutJokers; }
 
-		$hand = [];
-		foreach (str_split($handStr) as $card) {
-			$hand[] = CardType::from($card);
-		}
+		$possibleHands = [];
 
-		$entries[] = ['hand' => $hand, 'type' => getHandType($hand), 'bid' => $bid, 'handStr' => $handStr];
-	}
+		foreach (CardType::cases() as $card) {
+			$testHand = $handWithoutJokers;
+			if (in_array($card, $handWithoutJokers)) {
+				$testHand[] = $card;
 
-	usort($entries, function($a, $b) {
-		if ($a['type'] === $b['type']) {
-			for ($i = 0; count($a['hand']); $i++) {
-				if ($a['hand'][$i] != $b['hand'][$i]) {
-					return ($a['hand'][$i]->getNumericValue() <=> $b['hand'][$i]->getNumericValue());
+				if (count($testHand) == 5) {
+					$possibleHands[] = $testHand;
+				} else {
+					foreach (getPossibleHands($testHand) as $h) {
+						$possibleHands[] = $h;
+					}
 				}
 			}
-		} else {
-			return ($a['type']->getNumericValue() <=> $b['type']->getNumericValue());
 		}
 
-		echo 'Unable to sort: ', "\n";
-		var_dump($a);
-		var_dump($b);
-		die();
-	});
-
-	$part1 = 0;
-	foreach ($entries as $rank => $entry) {
-		$part1 += ($rank + 1) * $entry['bid'];
+		return $possibleHands;
 	}
 
+	function getHandsFromInput($allowJokers) {
+		global $input;
+
+		$hands = [];
+		foreach ($input as $line) {
+			preg_match('#(.*) (.*)#SADi', $line, $m);
+			[$all, $handStr, $bid] = $m;
+
+			$handWithoutJokers = $hand = [];
+			$jokers = 0;
+			foreach (str_split($handStr) as $card) {
+				if ($allowJokers && $card == 'J') {
+					$jokers++;
+					$card = '0';
+				}
+				$hand[] = CardType::from($card);
+				if ($card != 0) { $handWithoutJokers[] = CardType::from($card); }
+			}
+
+			$type = getHandType($hand);
+			$jokerHand = $hand;
+			if ($jokers == 5) {
+				$type = HandType::FiveOfAKind;
+			} else if ($jokers > 0) {
+				foreach (getPossibleHands($handWithoutJokers) as $testHand) {
+					$testType = getHandType($testHand);
+					if ($testType->getNumericValue() > $type->getNumericValue()) {
+						$type = $testType;
+						$jokerHand = $testHand;
+					}
+				}
+			}
+
+			$hands[] = ['hand' => $hand, 'type' => $type, 'bid' => $bid, 'jokers' => $jokers, 'jokerHand' => $jokerHand];
+		}
+
+		usort($hands, function($a, $b) {
+			if ($a['type'] === $b['type']) {
+				for ($i = 0; count($a['hand']); $i++) {
+					if ($a['hand'][$i] != $b['hand'][$i]) {
+						return ($a['hand'][$i]->getNumericValue() <=> $b['hand'][$i]->getNumericValue());
+					}
+				}
+			} else {
+				return ($a['type']->getNumericValue() <=> $b['type']->getNumericValue());
+			}
+
+			echo 'Unable to sort: ', "\n";
+			var_dump($a);
+			var_dump($b);
+			die();
+		});
+
+		return $hands;
+	}
+
+	$hands = getHandsFromInput(false);
+	if (isDebug()) {
+		echo "\n==========\n", "Part 1", "\n==========\n";
+		foreach ($hands as $rank => $hand) {
+			echo ($rank + 1), ' => ', json_encode($hand), "\n";
+		}
+		echo "==========\n";
+	}
+
+	$part1 = 0;
+	foreach ($hands as $rank => $entry) {
+		$part1 += ($rank + 1) * $entry['bid'];
+	}
 	echo 'Part 1: ', $part1, "\n";
+
+	$hands = getHandsFromInput(true);
+	if (isDebug()) {
+		echo "\n==========\n", "Part 2", "\n==========\n";
+		foreach ($hands as $rank => $hand) {
+			echo ($rank + 1), ' => ', json_encode($hand), "\n";
+		}
+		echo "==========\n";
+	}
+
+	$part2 = 0;
+	foreach ($hands as $rank => $entry) {
+		$part2 += ($rank + 1) * $entry['bid'];
+	}
+	echo 'Part 2: ', $part2, "\n";
