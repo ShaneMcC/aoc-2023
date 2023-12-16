aa2fa6ce8d2755ef779573058e876ea93d5c4cbf
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-16 06:01:52 +0000

    Tidy up a bit.

--- 16/run.php
+++ 16/run.php
@@ -3,7 +3,29 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$map = getInputMap();
 
+	$directions = [];
+	$directions['right'] = [1, 0];
+	$directions['left'] = [-1, 0];
+	$directions['down'] = [0, 1];
+	$directions['up'] = [0, -1];
+
+	$tiles = [];
+
+	$tiles['\\'] = ['left' => ['up'],
+	                'right' => ['down'],
+	                'down' => ['right'],
+	                'up' => ['left'],
+	               ];
+	$tiles['/'] = ['left' => ['down'],
+	               'right' => ['up'],
+	               'down' => ['left'],
+	               'up' => ['right'],
+	              ];
+	$tiles['-'] = ['down' => ['right', 'left'], 'up' => ['right', 'left']];
+	$tiles['|'] = ['right' => ['up', 'down'], 'left' => ['up', 'down']];
+
 	function getEnergised($map, $start = [-1, 0, 'right']) {
+		global $directions, $tiles;
 		$ends = [];
 
 		$ends[] = $start;
@@ -12,10 +34,8 @@
 		while (!empty($ends)) {
 			[$x, $y, $direction] = array_pop($ends);
 
-			if ($direction == 'right') { $x += 1; }
-			else if ($direction == 'left') { $x -= 1; }
-			else if ($direction == 'down') { $y += 1; }
-			else if ($direction == 'up') { $y -= 1; }
+			$x += $directions[$direction][0];
+			$y += $directions[$direction][1];
 
 			$cell = $map[$y][$x] ?? FALSE;
 			if ($cell !== FALSE) {
@@ -25,31 +45,9 @@
 					$visited["{$x},{$y}"][$direction] = true;
 				}
 
-				$direction2 = FALSE;
-				if ($cell == '\\') {
-					if ($direction == 'left') { $direction = 'up'; }
-					else if ($direction == 'right') { $direction = 'down'; }
-					else if ($direction == 'down') { $direction = 'right'; }
-					else if ($direction == 'up') { $direction = 'left'; }
-				} else if ($cell == '/') {
-					if ($direction == 'left') { $direction = 'down'; }
-					else if ($direction == 'right') { $direction = 'up'; }
-					else if ($direction == 'down') { $direction = 'left'; }
-					else if ($direction == 'up') { $direction = 'right'; }
-				} else if ($cell == '-') {
-					if ($direction == 'down' || $direction == 'up') {
-						$direction = 'right';
-						$direction2 = 'left';
-					}
-				} else if ($cell == '|') {
-					if ($direction == 'left' || $direction == 'right') {
-						$direction = 'down';
-						$direction2 = 'up';
-					}
+				foreach ($tiles[$cell][$direction] ?? [$direction] as $newDirection) {
+					$ends[] = [$x, $y, $newDirection];
 				}
-
-				$ends[] = [$x, $y, $direction];
-				if ($direction2 !== FALSE) { $ends[] = [$x, $y, $direction2]; }
 			}
 		}
 
