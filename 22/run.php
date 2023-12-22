47f51dcf7204a55c4266a05fcda8f33eb1574c63
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-22 06:21:04 +0000

    Day 22. Part 2. 1097.

--- 22/run.php
+++ 22/run.php
@@ -12,6 +12,27 @@
 		[$all, $x1, $y1, $z1, $x2, $y2, $z2] = $m;
 		$diff = [$x2 - $x1, $y2 - $y1, $z2 - $z1];
 
+		$bricks[$id] = ['location' => [intval($x1), intval($y1), intval($z1), intval($x2), intval($y2), intval($z2)], 'supports' => [], 'supportedBy' => [], 'fallen' => false];
+		addBrick($map, $bricks, $id);
+		$id++;
+	}
+	$bricks = updateSupports($map, $bricks);
+
+	function removeBrick(&$map, $bricks, $id) {
+		[$x1, $y1, $z1, $x2, $y2, $z2] = $bricks[$id]['location'];
+
+		for ($z = $z1; $z <= $z2; $z++) {
+			for ($y = $y1; $y <= $y2; $y++) {
+				for ($x = $x1; $x <= $x2; $x++) {
+					unset($map[$z][$y][$x]);
+				}
+			}
+		}
+	}
+
+	function addBrick(&$map, $bricks, $id) {
+		[$x1, $y1, $z1, $x2, $y2, $z2] = $bricks[$id]['location'];
+
 		for ($z = $z1; $z <= $z2; $z++) {
 			for ($y = $y1; $y <= $y2; $y++) {
 				for ($x = $x1; $x <= $x2; $x++) {
@@ -19,11 +40,7 @@
 				}
 			}
 		}
-
-		$bricks[$id] = ['location' => [intval($x1), intval($y1), intval($z1), intval($x2), intval($y2), intval($z2)], 'supports' => [], 'supportedBy' => []];
-		$id++;
 	}
-	$bricks = updateSupports($map, $bricks);
 
 	function supportedBy($map, $bricks, $id) {
 		if ($id == 'floor') { return ['floor']; }
@@ -69,6 +86,7 @@
 	}
 
 	function fall($map, $bricks) {
+		foreach (array_keys($bricks) as $id) { $bricks[$id]['fallen'] = false; }
 
 		$moved = false;
 		do {
@@ -76,33 +94,20 @@
 			foreach (array_keys($bricks) as $id) {
 				[$x1, $y1, $z1, $x2, $y2, $z2] = $bricks[$id]['location'];
 
-				$z = min($z1, $z2);
-
 				$supportedBy = supportedBy($map, $bricks, $id);
 
 				if (empty($supportedBy)) {
 					$moved = true;
 
 					// Remove brick
-					for ($z = $z1; $z <= $z2; $z++) {
-						for ($y = $y1; $y <= $y2; $y++) {
-							for ($x = $x1; $x <= $x2; $x++) {
-								unset($map[$z][$y][$x]);
-							}
-						}
-					}
-
-					// Add brick back, 1 lower.
-					for ($z = $z1; $z <= $z2; $z++) {
-						for ($y = $y1; $y <= $y2; $y++) {
-							for ($x = $x1; $x <= $x2; $x++) {
-								$map[$z - 1][$y][$x] = $id;
-							}
-						}
-					}
+					removeBrick($map, $bricks, $id);
 
 					// Update bricks array
 					$bricks[$id]['location'] = [$x1, $y1, $z1 - 1, $x2, $y2, $z2 - 1];
+					$bricks[$id]['fallen'] = true;
+
+					// Add brick back
+					addBrick($map, $bricks, $id);
 				}
 			}
 		} while ($moved);
@@ -144,5 +149,20 @@
 
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = 0;
+	foreach (array_keys($settledBricks) as $id) {
+		if (isDebug()) { echo 'Removing brick ', $id; }
+		$testMap = $settledMap;
+		removeBrick($testMap, $settledBricks, $id);
+		[$testMap, $testBricks] = fall($testMap, $settledBricks);
+
+		$fallCount = 0;
+		foreach ($testBricks as $b) {
+			if ($b['fallen']) { $fallCount++; }
+		}
+
+		$part2 += $fallCount;
+		if (isDebug()) { echo ' causes ', $fallCount, ' bricks to fall.', "\n"; }
+	}
+
+	echo 'Part 2: ', $part2, "\n";
